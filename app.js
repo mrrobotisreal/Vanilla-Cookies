bfe647dcbe3f7557a21f97282baba2db3a77f271
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-10 16:43:18 -0600

    Added app.js, put switch statement in it to monitor the readyState of the document, wrote the first case for "loading" and began to write out vanilla js for a loading bar.

---
+++ app.js
@@ -0,0 +1,7 @@
+switch (document.readyState) {
+  case "loading":
+    let loadingProgress = document.createElement('div');
+    let loadingBar = document.createElement('div');
+    loadingProgress.setAttribute('id', 'loadingProgress');
+    loadingBar.setAttribute('id', 'loadingBar');
+}
