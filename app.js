0340b2355be3725b27a699003bf2dbdc4cb1dcb1
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-10 18:27:17 -0600

    Finished loading circle, but it's currently set with setTimeout. Still need to figure out how to directly link it up with readyState since the while loop is not working.

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-switch (document.readyState) {
-  case "loading":
-    let loadingProgress = document.createElement('div');
-    let loadingBar = document.createElement('div');
-    loadingProgress.setAttribute('id', 'loadingProgress');
-    loadingBar.setAttribute('id', 'loadingBar');
-}
-
-let circle = document.createElement('div')
+let circle = document.createElement('div');
 circle.setAttribute('id', 'loadingProgress');
 document.getElementById('app').appendChild(circle);
+
+setTimeout(() => {
+  document.getElementById('app').removeChild(circle);
+  unchanged = false;
+}, 1500);
+
